e5ec29356a9ede1b8b8e9d21d64a206ce21f2391
fry13 <ninetailed1995@gmail.com>
2020-05-15 15:54:34 +0400

    исправить ошибки

--- js.js
+++ js.js
@@ -1,28 +1,32 @@
 const popup = document.querySelector('.popup');
-const editProfile = document.querySelector('.profile__button_edit');
-const addPhoto = document.querySelector('.profile__button_add');
-const name = document.querySelector('.profile__name');
-const bio = document.querySelector('.profile__bio');
-const inputName = document.querySelector('.popup__input_name');
-const inputBio = document.querySelector('.popup__input_bio');
+const editProfile = document.querySelector('.profile__button-edit');
+const addPhoto = document.querySelector('.profile__button-add');
+let name = document.querySelector('.profile__name');
+let bio = document.querySelector('.profile__bio');
+let inputName = document.querySelector('.popup__input_name');
+let inputBio = document.querySelector('.popup__input_bio');
 const popupClose = document.querySelector('.popup__exit');
-const form = document.querySelector('.popup__form-to-submit');
+const form = document.querySelector('.popup__form');
 
 //открытие/закрытие
 
 function popupToggle() {  
-  inputName.value = name.textContent;
-  inputBio.value = bio.textContent;
-  popup.classList.toggle('popup_visible');
+  if (popup.classList.contains('popup_visible') === 1) { //проверяем виден ли попап
+    popup.classList.toggle('popup_visible');             //если виден, скрываем
+  } else {
+    inputName.value = name.textContent;                  //если нет то перезаписываем 
+    inputBio.value = bio.textContent;                    //значения полей ввода
+    popup.classList.toggle('popup_visible');             //и показываем попап
+  }
 }
 
 //отправка
 
-function popupSubmit(evt) {
-  evt.preventDefault();
-  popup.classList.toggle('popup_visible');
-  name.textContent = inputName.value;
-  bio.textContent = inputBio.value;
+function popupSubmit(evt) {                              //
+  evt.preventDefault();                                  //
+  name.textContent = inputName.value;                    //перезаписываем имя и био используя
+  bio.textContent = inputBio.value;                      //значения из инпутов
+  popupToggle();                                         //вызываем фунцию открытия/закрытия
 }
 
 editProfile.addEventListener('click', popupToggle);
