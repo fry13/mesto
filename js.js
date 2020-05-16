4d292285ad4b79675b2142c8a4f666abd842b5f9
Alexey Feklyunin <fry13fry@yandex.ru>
2020-05-16 20:13:13 +0400

    сократить функцию

--- js.js
+++ js.js
@@ -10,15 +10,25 @@ const form = document.querySelector('.popup__form');
 
 //открытие/закрытие
 
+//function popupToggle() {  
+//  if (popup.classList.contains('popup_visible') === 1) { //проверяем виден ли попап
+//    popup.classList.toggle('popup_visible');             //если виден, скрываем
+//  } else {
+//    inputName.value = name.textContent;                  //если нет то перезаписываем 
+//    inputBio.value = bio.textContent;                    //значения полей ввода
+//    popup.classList.toggle('popup_visible');             //и показываем попап
+// }
+//}
+
+// я сократил функию примерно до такой, которую привели в пример вы,
+// но я буду настаивать, что моя фунция тоже работала :)
 function popupToggle() {  
-  if (popup.classList.contains('popup_visible') === 1) { //проверяем виден ли попап
-    popup.classList.toggle('popup_visible');             //если виден, скрываем
-  } else {
-    inputName.value = name.textContent;                  //если нет то перезаписываем 
-    inputBio.value = bio.textContent;                    //значения полей ввода
-    popup.classList.toggle('popup_visible');             //и показываем попап
-  }
-}
+  if (!popup.classList.contains('popup_visible')) {
+    inputName.value = name.textContent;                
+    inputBio.value = bio.textContent;    
+ } 
+    popup.classList.toggle('popup_visible');
+};
 
 //отправка
 
