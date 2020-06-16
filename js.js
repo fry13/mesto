6ca1b7ee0d00859c28e540e492f9c603284b71cc
Alexey Feklyunin <fry13fry@yandex.ru>
2020-06-16 22:17:26 +0400

    правки и рефакторинг

--- js.js
+++ js.js
@@ -1,13 +1,12 @@
 const popup = document.querySelector('.popup');
-const popupClose = document.querySelector('.popup__exit');
 const bioForm = document.forms.bio;
 const photoForm = document.forms.addPhoto;
 
 const editProfile = document.querySelector('.profile__button-edit');
 const addPhoto = document.querySelector('.profile__button-add');
 
-const name = document.querySelector('.profile__name');
-const bio = document.querySelector('.profile__bio');
+const profileName = document.querySelector('.profile__name');
+const profileBio = document.querySelector('.profile__bio');
 
 const formContainer = document.querySelector('.popup__form-with-cross_for-form');
 const photoContainer = document.querySelector('.popup__form-with-cross_for-photo');
@@ -21,7 +20,6 @@ const inputTitle = document.querySelector('.popup__input_title');
 const inputLink = document.querySelector('.popup__input_link');
 
 const cardContainer = document.querySelector('.elements');
-let card = document.querySelector('.elements__card');
 const cardTemplate = document.querySelector('#card-template').content; 
 
 
@@ -60,8 +58,8 @@ function popupEdit() {
   popup.classList.remove('popup_for-photo');                                                                    // уменьшаем затенение оверлея
   bioForm.classList.remove('popup__form_hidden');                                                               // прячем форму добавления фотокарточек.
   photoForm.classList.add('popup__form_hidden');                                                                // показываем форму для редактирования биографии
-  inputName.value = name.textContent;                                                                           // заполняем инпуты из био
-  inputBio.value = bio.textContent;        
+  inputName.value = profileName.textContent;                                                                           // заполняем инпуты из био
+  inputBio.value = profileBio.textContent;        
   popup.classList.add('popup_visible');                                                                         // показываем попап
   toggleButtonState(Array.from(bioForm.querySelectorAll('.popup__input')), bioForm.querySelector('.popup__save'));
   escHandler();
@@ -98,22 +96,27 @@ function popupAdd() {
 
 function cardCreate (cardTitle, cardLink) {              
   const newCard = cardTemplate.cloneNode(true);                                                                 // клонируем шаблон
-  
-  const like = newCard.querySelector('.elements__like');
-  like.addEventListener('click', () => like.classList.toggle('elements__like_active'));                         // слушатель на лайки
-  
-  const bin = newCard.querySelector('.elements__trash-bin');
-  bin.addEventListener('click', () => bin.parentElement.remove());                                              // слушатель на удаление
-
-  const photo = newCard.querySelector('.elements__photo');
-  photo.addEventListener('click', popupPhoto);                                                                  // слушатель для увеличения фото
- 
+   
   newCard.querySelector('.elements__title').textContent = cardTitle;                                            // берем имя из аргумента
   newCard.querySelector('.elements__photo').src = cardLink;                                                     // фото из аргумента
   newCard.querySelector('.elements__photo').alt = cardTitle;                                                    // alt из имени
-
+  
+  addEventListeners(newCard);
   return newCard;                                                                                               // возвращаем готовую карточку
 };
+
+// добавление слушателей на карточку
+
+function addEventListeners (card) {
+  const like = card.querySelector('.elements__like');
+  like.addEventListener('click', () => like.classList.toggle('elements__like_active'));                         // слушатель на лайки
+  
+  const bin = card.querySelector('.elements__trash-bin');
+  bin.addEventListener('click', () => bin.parentElement.remove());                                              // слушатель на удаление
+
+  const photo = card.querySelector('.elements__photo');
+  photo.addEventListener('click', popupPhoto);                                                                  // слушатель для увеличения фото
+}
 
 // закрытие по Esc
 
@@ -128,8 +131,8 @@ function escHandler() {
 
 // стандартные карточки
 
-initialCards.forEach( (i) => {
-  cardContainer.append(cardCreate(i.name, i.link));
+initialCards.forEach( (card) => {
+  cardContainer.append(cardCreate(card.name, card.link));
 });
 
 // слушатели кнопок
@@ -139,8 +142,8 @@ addPhoto.addEventListener('click', popupAdd);
 
 bioForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  name.textContent = inputName.value;
-  bio.textContent = inputBio.value;
+  profileName.textContent = inputName.value;
+  profileBio.textContent = inputBio.value;
   popup.classList.remove('popup_visible');
 });
 
